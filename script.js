cf68faaf865719fe9743f5c8b1f06d1da40f24d6
Giorgi Kostava <gkostava03@gmail.com>
2025-11-20 03:19:23 +0400

    created html/js file for homework 18

---
+++ script.js
@@ -0,0 +1,58 @@
+let age = Number(prompt("Enter your age"));
+let movieType = prompt("Regular or 3D").toLowerCase();
+let time = prompt("Morning or Evening").toLowerCase();
+
+function first() {
+  let havesaleticket = prompt("Do you have sale ticket").toLowerCase();
+  let price = 0;
+
+  switch (true) {
+    case age < 10:
+      price = 5;
+      break;
+
+    case age >= 10 && age < 65:
+      price = 15;
+      break;
+
+    case age >= 65:
+      price = 10;
+      break;
+
+    default:
+      alert("Invalid age");
+      return;
+  }
+
+  switch (movieType) {
+    case "3d":
+      price = price + 5;
+      break;
+    case "regular":
+      break;
+    default:
+      alert("Invalid movie type");
+      return;
+  }
+
+  switch (time) {
+    case "evening":
+      price = price + 5;
+      break;
+    case "morning":
+      break;
+    default:
+      alert("Invalid time");
+      return;
+  }
+
+  {
+    if (havesaleticket === "yes") {
+      price = price * 0.75;
+    } else {
+    }
+  }
+  alert("ბილეთის ფასი თქვენთვის არის: " + price + " ლარი.");
+}
+
+document.getElementById("btn").addEventListener("click", first);
